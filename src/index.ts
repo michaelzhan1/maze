247fd54dc8ec4a461dc2e246601890757bb972eb
Michael Zhan <michaelzhan2001@gmail.com>
2025-10-08 07:08:23 -0400

    add start + end + indexing

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ const MAX_SIZE = 50;
 
 const form = document.getElementById("maze-form") as HTMLFormElement;
 const maze = document.getElementById("maze") as HTMLDivElement;
+const mazeContainer = document.getElementById(
+  "maze-container"
+) as HTMLDivElement;
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -23,6 +26,18 @@ form.addEventListener("submit", (e) => {
 });
 
 function generateMaze(size: number): void {
+  mazeContainer.innerHTML = "";
+  const start = document.createElement("div");
+  start.classList.add("flex-start");
+  start.textContent = "Start";
+  const end = document.createElement("div");
+  end.classList.add("flex-end");
+  end.textContent = "End";
+
+  mazeContainer.appendChild(start);
+  mazeContainer.appendChild(maze);
+  mazeContainer.appendChild(end);
+
   maze.innerHTML = "";
 
   for (let i = 0; i < size; i++) {
@@ -32,9 +47,11 @@ function generateMaze(size: number): void {
     for (let j = 0; j < size; j++) {
       const col = document.createElement("div");
       col.classList.add("col");
+      col.setAttribute("i", i.toString());
+      col.setAttribute("j", j.toString());
       row.appendChild(col);
     }
 
-    maze.appendChild(row); 
+    maze.appendChild(row);
   }
 }
